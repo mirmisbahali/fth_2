1643705ac71788e2220d07d7e6dad9df1178c278
Mir Misbah Ali <mirmisbahali@gmail.com>
2020-08-14 01:50:42 +0530

    Done all the revisions

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const progress = document.querySelector('.progress-done');
-const progressText = document.querySelector('.progress_text');
+const progressText = document.querySelector('.progress_text .percentage');
 
 
 progress.style.width = progress.getAttribute('data-done') + '%';
 progress.style.opacity = 1;
-progressText.innerHTML = progress.getAttribute('data-done') + '%';
+// progressText.innerHTML = `${progress.getAttribute('data-done')}%`;
+progressText.innerHTML = `${progress.getAttribute('data-done')}%<div class="stick"></div>`;
+
 progressText.style.left = progress.getAttribute('data-done') + '%';
 
 if (progress.getAttribute('data-done') == 100) {
